e2f1523f40643a4292d9fe6ea0998ed9bef7746f
DenisAvram98 <denis_avram98@yahoo.com>
2021-03-30 17:32:22 +0200

    Add project files.

---
+++ Server/PoartaLogica.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Data;
+
+namespace Server
+{
+    internal class PoartaLogica
+    {
+        internal static int CalculRezultat(ref DataTable dt)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                IncarcareSiCalculValori(dr, ref dt);
+            }
+
+            //cautam elementul logic OUTPUT si returnam OutputResut
+            foreach(DataRow dr in dt.Rows)
+            {
+                int CT = Int32.Parse(dr["CurrentType"].ToString());
+                int OR = dr["OutputResult"] is "Server" ? Int16.MaxValue : Int32.Parse(dr["OutputResult"].ToString());
+
+                if (CT==6)
+                {
+                    return OR;
+                }
+            }
+            return Int16.MaxValue; //eroare, nu sa gasit rezultatul
+        }
+
+        private static void IncarcareSiCalculValori(DataRow dr, ref DataTable dt)
+        {
+            //Inexistent = Int16.MinValue; Server= Int16.MaxValue;
+            int FSID = dr["FirstSourceID"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["FirstSourceID"].ToString());
+            int SSID = dr["SecondSourceID"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["SecondSourceID"].ToString());
+            int FST = dr["FirstSourceType"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["FirstSourceType"].ToString());
+            int SST = dr["SecondSourceType"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["SecondSourceType"].ToString());
+            int DID = dr["DestinationID"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["DestinationID"].ToString());
+            int DT = dr["DestinationType"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["DestinationType"].ToString());
+            int CID = Int32.Parse(dr["CurrentID"].ToString());
+            int CT = Int32.Parse(dr["CurrentType"].ToString());
+            int OR = dr["OutputResult"] is "Server" ? Int16.MaxValue : Int32.Parse(dr["OutputResult"].ToString());
+            int FI = dr["FirstInput"] is "Server" ? Int16.MaxValue : Int32.Parse(dr["FirstInput"].ToString());
+            int SI = dr["SecondInput"] is "Server" ? Int16.MaxValue : Int32.Parse(dr["SecondInput"].ToString());
+            string IID = dr["InputID"] is DBNull ? "Inexistent" : dr["InputID"].ToString();
+            string GN = dr["GateName"].ToString();
+
+            bool bFI = Convert.ToBoolean(FI);
+            bool bSI = Convert.ToBoolean(SI);
+            bool bOR;
+
+            if (CT == 4 || CT == 5) //Input 0, 1
+            {
+                return;
+            }
+
+            if (FST == 4 || FST == 5)
+            {
+                dr.BeginEdit();
+                dr["FirstInput"] = FST is 4 ? 0 : 1;
+                dr.EndEdit();
+            }
+
+            if (SST == 4 || SST == 5)
+            {
+                dr.BeginEdit();
+                dr["SecondInput"] = SST is 4 ? 0 : 1;
+                dr.EndEdit();
+            }
+
+            if ((FST == 4 || FST == 5) && (SST == 4 || SST == 5))
+            {
+                bool value1 = Convert.ToBoolean(FST is 4 ? 0 : 1);
+                bool value2 = Convert.ToBoolean(SST is 4 ? 0 : 1);
+
+                bOR = Operatii(CT, value1, value2);
+
+                dr.BeginEdit();
+                dr["OutputResult"] = Convert.ToInt32(bOR);
+                dr.EndEdit();
+
+                //facem update cu output result unde aceasta poarta este input
+                UpdateOutputResultInAltePortiInput(ref dt, CID, bOR);
+            }
+            else if ((CT == 3 || CT == 6) && (FST == 4 || FST == 5)) //NOT, Output && Input 0, 1
+            {
+                bool value1 = Convert.ToBoolean(FST is 4 ? 0 : 1);
+                bool value2 = Convert.ToBoolean(SST is 4 ? 0 : 1);
+
+                bOR = Operatii(CT, value1, value2);
+
+                dr.BeginEdit();
+                dr["OutputResult"] = Convert.ToInt32(bOR);
+                dr.EndEdit();
+
+                //facem update cu output result unde aceasta poarta este input
+                UpdateOutputResultInAltePortiInput(ref dt, CID, bOR);
+            }
+            //prima conditie sa fie indeplinita FirstInput trebuie sa fie initializat
+            //cea de a doua conditie sa fie indeplinita trebuie ca FirstInput si SecondInput sa fie initializate
+            //cazul pentru restul tipurilor de input (excludem Input 0, 1)
+            else if (((CT == 3 || CT == 6) && FI != Int16.MaxValue) || (FI != Int16.MaxValue && SI != Int16.MaxValue))
+            {
+                bOR = Operatii(CT, bFI, bSI);
+
+                dr.BeginEdit();
+                dr["OutputResult"] = Convert.ToInt32(bOR);
+                dr.EndEdit();
+
+                //facem update cu output result unde aceasta poarta este input
+                UpdateOutputResultInAltePortiInput(ref dt, CID, bOR);
+            }
+            else //cand poarta curenta are ca intrari ce trebuie mai intai calculate si apoi se calculeaza outputul porti curente
+            {
+                DataRow FirstInputRow = null;
+                DataRow SecondInputRow = null;
+
+                foreach(DataRow dr1 in dt.Rows)
+                {
+                    int CurrentID = Int32.Parse(dr1["CurrentID"].ToString());
+                    if (CurrentID==FSID)
+                    {
+                        FirstInputRow = dr1;
+                    }
+                    if (CurrentID==SSID)
+                    {
+                        SecondInputRow = dr1;
+                    }
+                }
+
+                //calculam intrarile
+                if (CT==3 || CT==6) //NOT, Output
+                {
+                    IncarcareSiCalculValori(FirstInputRow, ref dt);
+                    IncarcareSiCalculValori(dr, ref dt);
+                }
+                else //restul elementelor logige
+                {
+                    if (!(FST==4 || FST==5))
+                    {
+                        IncarcareSiCalculValori(FirstInputRow, ref dt);
+                    }
+                    if (!(SST==4 || SST==5))
+                    {
+                        IncarcareSiCalculValori(SecondInputRow, ref dt);
+                    }
+                    //IncarcareSiCalculValori(dr, ref dt);
+                }
+            }
+        }
+
+        private static void UpdateOutputResultInAltePortiInput(ref DataTable dt, int cID, bool bOR)
+        {
+            //facem update cu output result unde aceasta poarta este input
+            foreach (DataRow dr in dt.Rows)
+            {
+                //string test = dr["FirstSourceID"].ToString();
+                int FSID = dr["FirstSourceID"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["FirstSourceID"].ToString());
+                int SSID = dr["SecondSourceID"].ToString() is "Inexistent" ? Int16.MinValue : Int32.Parse(dr["SecondSourceID"].ToString());
+                int CID = Int32.Parse(dr["CurrentID"].ToString());
+
+                if (((FSID==cID) || (SSID==cID)) && (CID!=cID))
+                {
+                    dr.BeginEdit();
+                    if (FSID==cID)
+                    {
+                        dr["FirstInput"] = Convert.ToInt32(bOR);
+                    }
+                    if (SSID==cID)
+                    {
+                        dr["SecondInput"] = Convert.ToInt32(bOR);
+                    }
+                    dr.EndEdit();
+                }
+            }
+        }
+
+        private static bool Operatii(int cT, bool value1, bool value2)
+        {
+            switch (cT)
+            {
+                case 0: //AND
+                    return value1 & value2;
+                case 1: //OR
+                    return value1 | value2;
+                case 2: //XOR
+                    return value1 ^ value2;
+                case 3: //NOT
+                    return !value1;
+                case 6: //OUTPUT
+                    return value1;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAdress = "192.168.0.3";
+            Socket ServerListner = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAdress), port);
+            ServerListner.Bind(ep);
+            ServerListner.Listen(100); //server is listening
+            Console.WriteLine("Server is listening...");
+
+            //client
+            Socket ClientSocket = default(Socket);
+            int count = 0;
+            Program p = new Program();
+            while (true)
+            {
+                count++;
+                ClientSocket = ServerListner.Accept();
+                Console.WriteLine(count + " Clients connected");
+                Thread UserThread = new Thread(new ThreadStart(() => p.User(ClientSocket, Thread.CurrentThread))); //for every client difderent thread
+                UserThread.Start();
+            }
+        }
+
+        public static byte[] ReadToEnd(NetworkStream stream)
+        {
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                    if (stream.DataAvailable)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+
+        public void User(Socket client, Thread userThread) //repplaying function
+        {
+            while (true)
+            {
+                NetworkStream stream = new NetworkStream(client);
+                byte[] d = ReadToEnd(stream);
+                stream.Close();
+                //Console.WriteLine("Received: " + dataStr);
+
+                DataTable dt = null;
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                MemoryStream mStream = new MemoryStream(d);
+                //mStream.Seek(0, SeekOrigin.Begin);
+                if (mStream.Length!=0)
+                {
+                    dt = (DataTable)bFormatter.Deserialize(mStream);
+                }
+                mStream.Close();
+                if (dt != null)
+                {
+                    /*
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("Client ID: " + userThread.GetHashCode().ToString());
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine("----------------------");
+                        Console.WriteLine("FSID: " + row["FirstSourceID"]);
+                        Console.WriteLine("SSID: " + row["SecondSourceID"]);
+                        Console.WriteLine("FST: " + row["FirstSourceType"]);
+                        Console.WriteLine("SST: " + row["SecondSourceType"]);
+                        Console.WriteLine("DID: " + row["DestinationID"]);
+                        Console.WriteLine("DT: " + row["DestinationType"]);
+                        Console.WriteLine("CID: " + row["CurrentID"]);
+                        Console.WriteLine("CT: " + row["CurrentType"]);
+                        Console.WriteLine("OR: " + row["OutputResult"]);
+                        Console.WriteLine("FI: " + row["FirstInput"]);
+                        Console.WriteLine("SI: " + row["SecondInput"]);
+                        Console.WriteLine("IID: " + row["InputID"]);
+                        Console.WriteLine("GN: " + row["GateName"]);
+                    }
+                    */
+                    //Calculare rezultat
+                    int rezultat = PoartaLogica.CalculRezultat(ref dt);
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("Client ID: " + userThread.GetHashCode().ToString() + " \nRezultat: " + rezultat);
+                    /*foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine("----------------------");
+                        Console.WriteLine("FSID: " + row["FirstSourceID"]);
+                        Console.WriteLine("SSID: " + row["SecondSourceID"]);
+                        Console.WriteLine("FST: " + row["FirstSourceType"]);
+                        Console.WriteLine("SST: " + row["SecondSourceType"]);
+                        Console.WriteLine("DID: " + row["DestinationID"]);
+                        Console.WriteLine("DT: " + row["DestinationType"]);
+                        Console.WriteLine("CID: " + row["CurrentID"]);
+                        Console.WriteLine("CT: " + row["CurrentType"]);
+                        Console.WriteLine("OR: " + row["OutputResult"]);
+                        Console.WriteLine("FI: " + row["FirstInput"]);
+                        Console.WriteLine("SI: " + row["SecondInput"]);
+                        Console.WriteLine("IID: " + row["InputID"]);
+                        Console.WriteLine("GN: " + row["GateName"]);
+                    }*/
+
+                    DataTable dt2 = new DataTable();
+                    DataColumn dc;
+
+                    //template tabel nou
+                    dc = new DataColumn();
+                    dc.ColumnName = "FirstInput";
+                    dc.DataType = Type.GetType("System.String");
+                    dt2.Columns.Add(dc);
+
+                    dc = new DataColumn();
+                    dc.ColumnName = "SecondInput";
+                    dc.DataType = Type.GetType("System.String");
+                    dt2.Columns.Add(dc);
+
+                    dc = new DataColumn();
+                    dc.ColumnName = "GateName";
+                    dc.DataType = Type.GetType("System.String");
+                    dt2.Columns.Add(dc);
+
+                    dc = new DataColumn();
+                    dc.ColumnName = "OutputResult";
+                    dc.DataType = Type.GetType("System.String");
+                    dt2.Columns.Add(dc);
+
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (Int32.Parse(dr["CurrentType"].ToString())!=4 && Int32.Parse(dr["CurrentType"].ToString()) != 5 && Int32.Parse(dr["CurrentType"].ToString()) != 6) //diferit de Input 0, 1, Output
+                        {
+                            DataRow dr1 = dt2.NewRow();
+                            dr1["FirstInput"] = dr["FirstInput"];
+                            dr1["SecondInput"] = dr["SecondInput"].ToString() is "Server" ? "Inexistent" : dr["SecondInput"];
+                            dr1["GateName"] = dr["GateName"].ToString() + " | ID: " + dr["CurrentID"].ToString();
+                            dr1["OutputResult"] = dr["OutputResult"];
+                            dt2.Rows.Add(dr1);
+                        }
+                    }
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (Int32.Parse(dr["CurrentType"].ToString())==6)
+                        {
+                            DataRow dr1 = dt2.NewRow();
+                            dr1["FirstInput"] = dr["FirstInput"];
+                            dr1["SecondInput"] = dr["SecondInput"].ToString() is "Server" ? "Inexistent" : dr["SecondInput"];
+                            dr1["GateName"] = dr["GateName"].ToString() + " | ID: " + dr["CurrentID"].ToString();
+                            dr1["OutputResult"] = dr["OutputResult"];
+                            dt2.Rows.Add(dr1);
+                        }
+                    }
+                    foreach (DataRow row in dt2.Rows)
+                    {
+                        Console.WriteLine("----------------------");
+                        Console.WriteLine("FI: " + row["FirstInput"]);
+                        Console.WriteLine("SI: " + row["SecondInput"]);
+                        Console.WriteLine("GN: " + row["GateName"]);
+                        Console.WriteLine("OR: " + row["OutputResult"]);
+                    }
+
+                    //raspuns catre client
+                    mStream = new MemoryStream();
+                    dt2.RemotingFormat = SerializationFormat.Binary;
+                    bFormatter.Serialize(mStream, dt2);
+
+                    byte[] b = mStream.ToArray();
+                    mStream.Close();
+
+                    if (client.Poll(1000, SelectMode.SelectRead) && client.Available == 0)
+                    {
+                        Console.WriteLine("Clientul ID: "+ userThread.GetHashCode().ToString() + " sa deconectat de la server.");
+                    }
+                    else
+                    {
+                        client.Send(b, 0, b.Length, SocketFlags.None);
+                    }
+                }
+            }
+        }
+    }
+}
